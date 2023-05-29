429b66619659c21f010fa4ed342048c2c4ce8b40
Trubnikov <kawaiizdesu@mail.ru>
2023-05-30 00:38:48 +0700

    asd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Введите а");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            b = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("Выбор:");
+                Console.WriteLine("1.Ввести а");
+                Console.WriteLine("2.Ввести б");
+                Console.WriteLine("3.выполнить операцию +");
+                Console.WriteLine("4.выполнить операцию -");
+                Console.WriteLine("5.выполнить операцию *");
+                Console.WriteLine("6. выполнить операцию “/”");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+
+                        break;
+                    case "6":
+
+                        break;
+                }
+            }
+
+        }
+    }
+
+}
